69e33ec73934a2abb94f5928d3b9a18099ebd382
vishnukumar <vishnukumar@goodearth.in>
2017-03-12 17:17:12 +0530

    Updated geodata and carrier data

---
+++ python/phonenumbers/test.py
@@ -0,0 +1,3 @@
+import phonenumbers
+x = phonenumbers.parse("+917400397070", None)
+print x
--- python/tests/testtzdata/data0.py
+++ python/tests/testtzdata/data0.py
@@ -20,13 +20,13 @@ from phonenumbers.util import u
 
 data = {
  '1234':('America/New_York',),
+ '1212812':('America/New_York',),
+ '1604':('America/Winnipeg',),
+ '82':('Asia/Seoul',),
  '612':('Australia/Sydney',),
+ '1617423':('America/Chicago',),
+ '1':('America/New_York', 'America/Chicago', 'America/Winnipeg', 'America/Los_Angeles'),
+ '1989':('Ameriac/Los_Angeles',),
  '1650960':('America/Los_Angeles',),
  '1201':('America/New_York',),
- '1604':('America/Winnipeg',),
- '1989':('Ameriac/Los_Angeles',),
- '1':('America/New_York', 'America/Chicago', 'America/Winnipeg', 'America/Los_Angeles'),
- '1212812':('America/New_York',),
- '1617423':('America/Chicago',),
- '82':('Asia/Seoul',),
 }
